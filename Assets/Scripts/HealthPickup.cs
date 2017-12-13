094335f5fa44a8113be2017de51a4656ef90f6d2
Ishup Khan <ishupk@metropolia.fi>
2017-12-13 02:10:27 +0200

    added health pick up scripts and health bar on player

---
+++ Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour 
+{
+    public int HealthToGive;
+
+    public AudioSource Pickupsound;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Player>() == null)
+            return;
+        
+        Healthmanager.Hurtplayer(-HealthToGive);
+
+        //Pickupsound.Play();
+
+        Destroy(gameObject);
+    }
+
+}
